9bd4076a909694be459b45ff30eb57c1d175e40d
Dotwood Media <55753961+DotwoodMedia@users.noreply.github.com>
2021-07-01 14:43:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,49 +4,91 @@ async function getStats() {
     const result = await fetch('https://api.dotwood.media/musictop20/stats');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getStatsUsers() {
     const result = await fetch('https://api.dotwood.media/musictop20/stats/users');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getStatsNominations() {
     const result = await fetch('https://api.dotwood.media/musictop20/stats/nominations');
 
     const data = await result.json();
-    return data;
+
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getStatsVotes() {
     const result = await fetch('https://api.dotwood.media/musictop20/stats/votes');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getStatsViews() {
     const result = await fetch('https://api.dotwood.media/musictop20/stats/views');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getNominations() {
     const result = await fetch('https://api.dotwood.media/musictop20/nominations');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function getEdition1() {
     const result = await fetch('https://api.dotwood.media/musictop20/edition1');
 
     const data = await result.json();
-    return data;
+    
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function register(api, username, email, pwd) {
@@ -57,7 +99,13 @@ async function register(api, username, email, pwd) {
     const result = await fetch(`https://api.dotwood.media/musictop20/register?key=${api}?username=${username}&email=${email}&pwd=${pwd}`);
 
     const data = await result.json();
-    return data;
+
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 async function mynominations(api) {
@@ -65,7 +113,13 @@ async function mynominations(api) {
     const result = await fetch(`https://api.dotwood.media/musictop20/mynominations?key=${api}`);
 
     const data = await result.json();
-    return data;
+
+    if (data.error == true) {
+        throw new Error(data.message);
+    }
+    else {
+        return data;
+    }
 }
 
 module.exports = { getStats, getNominations, getStatsUsers, getStatsNominations, getStatsVotes, getStatsViews, getEdition1, register, mynominations };
